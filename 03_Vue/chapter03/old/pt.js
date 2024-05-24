39b90bd2bd2c675d971490db9a2db11f51332ec7
SeungA Lee <dltmddk981119@gmail.com>
2024-05-24 18:01:09 +0900

    20240524 부트스트랩 첫째날, 로그인화면 만들어봄

---
+++ 03_Vue/chapter03/old/pt.js
@@ -0,0 +1,25 @@
+// const a = 1;
+// const b = [2, 3, 4, 5, 6];
+// console.log(a);
+// console.log(b);
+
+const a = 1;
+const b = 1;
+// true
+console.log(a === b);
+// true
+console.log(a == b);
+
+const c = 1;
+const d = '1';
+// false
+console.log(c === d);
+// true
+console.log(c == d);
+
+const e = [1, 2, 3, 4, 5];
+const f = [1, 2, 3, 4, 5];
+// false
+console.log(e == f);
+// true
+console.log(e[0] === f[0]);
